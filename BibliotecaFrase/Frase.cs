d91158c5d4e6cfea6c1d8003cb6705d11af36a91
ReneFuentes26 <fuentesbenitezreneismael@gmail.com>
2021-08-19 10:36:17 -0600

    Agregar archivos de proyecto.

---
+++ BibliotecaFrase/Frase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaFrase
+{
+    public class Frase
+    {
+        public static void SortedListCollection()
+        {
+            var letras = new[] { 'A', 'E', 'I', 'O', 'U' };
+
+            Console.WriteLine("Introduce un texto: ");
+            var text = Console.ReadLine().ToUpper();
+
+            foreach (var v in letras)
+            {
+                var vocales = text.Count(x => x == v);
+                Console.WriteLine($"Existen {vocales} vocales {v}");
+            }
+        }
+     
+        }
+    }
+
